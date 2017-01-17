7d76b56ec7906e6a959008a037dc573041bbc36b
huangqi <huangqi200708@163.com>
2017-01-17 10:38:37 +0800

    modify

---
+++ myPython0.1/cn/hq/class/MemberCount.py
@@ -0,0 +1,18 @@
+'''
+Created on 2017年1月17日
+
+@author: 黄淇
+'''
+
+class MemberCount(object):
+    '''
+    classdocs
+    '''
+    members = 0;
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+    def count(self):
+         MemberCount.members +=1
---
+++ myPython0.1/cn/hq/test/helloword.py
@@ -0,0 +1,6 @@
+'''
+Created on 2016年12月8日
+
+@author: 黄淇
+'''
+print("hello word")
