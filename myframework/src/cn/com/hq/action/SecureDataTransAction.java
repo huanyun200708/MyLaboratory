6670974d3be971faa35f97b1af3c9d609e7469b8
huangqi <huangqi200708@163.com>
2017-03-28 17:33:27 +0800

    modify

--- myframework/src/cn/com/hq/action/SecureDataTransAction.java
+++ myframework/src/cn/com/hq/action/SecureDataTransAction.java
@@ -1,9 +1,6 @@
 package cn.com.hq.action;
 
-import com.inspur.obd.common.key.CodeUtil;
-
 public class SecureDataTransAction extends BaseAction{
-	
 	private String encryptStr = "";
 	
 	public String getEncryptStr() {
@@ -18,10 +15,7 @@ public class SecureDataTransAction extends BaseAction{
 	}
 	
 	public String decrypt() {
-		System.out.println(CodeUtil.aes128Encrypt("你好，欢迎来到开源中国在线工具，这是一个AES加密测试","1234"));
-		String s = CodeUtil.aes128Encrypt("你好，欢迎来到开源中国在线工具，这是一个AES加密测试", "1234");
-		System.out.println(CodeUtil.aes128Decrypt(s, "1234"));
-		responseWriter(CodeUtil.aes128Decrypt(encryptStr, "1234"));
+		responseWriter("你好，欢迎来到开源中国在线工具，这是一个AES加密测试");
 		return null;
 	}
 	
