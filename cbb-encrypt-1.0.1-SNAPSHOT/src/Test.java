9f18d2de3595693ef3c50622786f28345abd21f7
huangqi <huangqi@huangqi01.home.langchao.com>
2017-11-22 09:12:50 +0800

    add

--- cbb-encrypt-1.0.1-SNAPSHOT/src/Test.java
+++ cbb-encrypt-1.0.1-SNAPSHOT/src/Test.java
@@ -1,8 +1,11 @@
 import com.huawei.inoc.most.cbb.encrypt.AesEncrypt;
+import com.huawei.inoc.most.cbb.encrypt.ShaEncrypt;
 
 public class Test {
 	public static void main(String[] args) {
 		String encrypt = AesEncrypt.encrypt("aaa");
 		System.out.println(AesEncrypt.decrypt(encrypt));
+		System.out.println(ShaEncrypt.SALT);
+		System.out.println(ShaEncrypt.encrypt("aaa","WL0nookvZt14sPTg"));
 	}
 }
